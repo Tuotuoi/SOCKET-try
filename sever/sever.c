9eff15c836663e596c5059aed20985fd7c92e8e3
Tuotuoi <m18999624868_1@163.com>
2020-03-28 15:41:48 +0800

    second commit

--- sever/sever.c
+++ sever/sever.c
@@ -1,84 +1,63 @@
 /*************************************************************************
-	> File Name: sever.c
+	> File Name: sever1.c
 	> Author: 
 	> Mail: 
-	> Created Time: 2020年03月22日 星期日 20时04分47秒
+	> Created Time: 2020年03月28日 星期六 15时02分25秒
  ************************************************************************/
 
-#include "head.h"
+#include"head.h"
+#include"tcp_sever.h"
 
-void *recvive(void *arg) {
-    char name[20] = {0};
-    int *tmp;
-    tmp = (int *)arg;
-    if(recv(*tmp,name,sizeof(name),0) <= 0) {
-        perror("recv");
-        close(*tmp);
+void chstr(char *str) {
+    for(int i = 0; i < strlen(str);i++) {
+        if(str[i] >= 'a' && str[i] <= 'z') {
+            str[i] -= 32;
+        }
     }
-    printf("Name = %s\n",name);
-    int retval = 3;
-    pthread_exit((void *)&retval);
-    
 }
 
-
-int main(int argc, char **argv) {
-    if(argc != 2) {
-        fprintf(stderr,"Usage : %s port\n",argv[0]);
-        exit(1);
+void *work(void *arg) {
+    int *fd = (int *)arg;
+    if (send(fd,"You Are Here",sizeof("You Are Here"),0) < 0){
+        perror("send");
+        close(fd);
+        return NULL;
     }
-    int port, sever_listen;
+    while (1) {
+        char msg[512] = {0};
+        if (recv(fd,msg,sizeof(msg),0) <= 0) {
+            break;
+        }
+        printf("recv: %s\n",msg);
+        chstr(msg);
+        if(send(fd,msg,strlen(msg),0) < 0) {
+            perror("send");
+            break;
+        }
+
+    }
+    close(fd);
+    return NULL;
+}
+
+int main(int argc,char **argv) {
+    int port,sever_listen,fd;
+    if(argc != 2) {
+        fprintf(stderr,"Usage : %s port!\n",argv[0]);
+        return 1;
+    }
     port = atoi(argv[1]);
 
-    if ((sever_listen = socket(AF_INET,SOCK_STREAM,0)) < 0) {
-        perror("socket");
-        exit(1);
+    if((sever_listen = socket_create(port)) < 0) {
+        perror("socket_create");
+        return 2;
     }
 
-    struct sockaddr_in sever;
-    sever.sin_family = AF_INET;
-    sever.sin_port = htons(port);
-    sever.sin_addr.s_addr = INADDR_ANY;
-    if(bind(sever_listen,(struct sockaddr *)&sever,sizeof(sever)) < 0) {
-        perror("bind");
-        exit(1);
-    }
-    if(listen(sever_listen,20) < 0) {
-        perror("sever");
-        exit(1);
-    }
+    pthread_t tid;
     while(1) {
-        int sockfd;
-        if ((sockfd = accept(sever_listen,NULL,NULL)) < 0) {
+        if((fd = accept(sever_listen,NULL,NULL)) < 0) {
             perror("accept");
-            close(sockfd);
-            continue;
         }
-
-       /* pid_t pid;
-        if((pid = fork()) < 0) {
-            perror("fork");
-            continue;
-        }
-
-        if(pid == 0) {
-            close(sever_listen);
-            char name[20] = {0};
-            if(recv(sockfd,name,sizeof(name),0) <= 0) {
-                perror("recv");
-                close(sockfd);
-            }
-            printf("Name = %s\n",name);
-            exit(0);
-        }*/
-        void *status;
-
-        pthread_t tid;
-        pthread_create(&tid,NULL,recvive,(void *)&sockfd);
-        pthread_join(tid,&status);
-        printf("%d\n",*(int*)status);
-        sleep(1);
-
+        pthread_create(&tid,NULL,work,(void *)&fd);
     }
-    return 0;
 }
